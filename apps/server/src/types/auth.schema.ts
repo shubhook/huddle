30c63fc18fa01cb516f77e008af61c4222502fdf
shubhook <khakhashubham@gmail.com>
2026-06-06 20:55:23 +0530

    chore: separate signup and signin schemas

--- apps/server/src/types/auth.schema.ts
+++ apps/server/src/types/auth.schema.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
-export const authSchema = z.object({
+export const signinSchema = z.object({
     username: z.string().min(1, 'username is required'),
+    password: z.string().min(1, 'password is required'),
+    email:    z.string().min(1, 'email is required')
+});
+
+export const signupSchema = z.object({
     password: z.string().min(1, 'password is required'),
     email:    z.string().min(1, 'email is required')
 });
