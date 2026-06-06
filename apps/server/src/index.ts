ec2e06de3a0b6353280ceb1fca19c820ed0e913b
shubhook <khakhashubham@gmail.com>
2026-06-06 20:31:38 +0530

    feat: add src/index.ts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from "express"
+import { env } from "./utils/env";
+import { appRouter } from "./routes";
+
+const app = express();
+
+app.use(express());
+
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: "ok"
+    });
+});
+
+app.use(appRouter);
+
+app.listen(env.PORT, () => {
+    console.log(`Server is running at http://localhost:${env.PORT}`);
+})
