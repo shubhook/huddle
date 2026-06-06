570b96139058ca4b3eddf52d907d9f3c1667f152
shubhook <khakhashubham@gmail.com>
2026-06-06 20:25:06 +0530

    feat: add generateToken and requireAuth function for email login

---
+++ apps/server/src/utils/auth.ts
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import  type { Request, Response, NextFunction } from "express"
+import { env } from "./env";
+
+export type tokenPayload = {
+    userId: string
+}
+
+export function generateToken(payload: tokenPayload): string {
+    return jwt.sign(payload, env.JwtSecret);
+}
+
+export async function requireAuth(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(token == undefined) {
+        res.status(401).json({
+            message: "<Missing Token>"
+        });
+        return;
+    }
+
+    try {
+        const decoded = await jwt.verify(token, env.JwtSecret) as tokenPayload;
+        req.userId = decoded.userId;
+        next();
+        
+    } catch(err) {
+        console.log(err);
+        res.status(403).json({
+            mesaage: "Unauthorised Endpoint"
+        });
+        return;
+    }
+}
