1f244de53285498423285723f45c93919b8cc643
shubhook <khakhashubham@gmail.com>
2026-06-12 21:46:06 +0530

    feat: add channelRouter

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authRouter } from "./auth.routes";
 import { workspaceRouter } from "./workspace.routes";
+import { channelRouter } from "./channel.routes";
 
 export const appRouter = Router();
 
 appRouter.use(authRouter);
 appRouter.use(workspaceRouter);
+appRouter.use(channelRouter);
