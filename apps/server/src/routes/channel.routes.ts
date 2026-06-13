a518529c8df2aca88c23c9b31f836e8ab8bdd4fe
shubhook <khakhashubham@gmail.com>
2026-06-13 10:16:18 +0530

    feat: add POST route for sendMessage and type for message-content

--- apps/server/src/routes/channel.routes.ts
+++ apps/server/src/routes/channel.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { requireAuth } from "../utils/auth";
 import { workspaceAuth } from "../middleware/workspace.middleware";
 import { asyncHandler } from "../utils/async-handler";
-import { createChannel, getAllChannels, getMessages } from "../controllers/channel.controller"
+import { createChannel, getAllChannels, getMessages, sendMessages } from "../controllers/channel.controller"
 import { channelAuth } from "../middleware/channel.middleware";
 
 export const channelRouter = Router();
@@ -10,3 +10,4 @@ export const channelRouter = Router();
 channelRouter.post('/workspaces/:id/channels', requireAuth, workspaceAuth, asyncHandler(createChannel));
 channelRouter.get('/workspaces/:id/channels', requireAuth, workspaceAuth, asyncHandler(getAllChannels));
 channelRouter.get('/channels/:id/messages', requireAuth, channelAuth, asyncHandler(getMessages));
+channelRouter.post('/channel/:id/messages', requireAuth, channelAuth, asyncHandler(sendMessages));
--- apps/server/src/types/request.schema.ts
+++ apps/server/src/types/request.schema.ts
@@ -6,4 +6,8 @@ export const new_workspace_schema = z.object({
 
 export const new_channel_schema = z.object({
     name: z.string().min(1, "name is required")
-})
+});
+
+export const message_schema = z.object({
+    content: z.string().min(1, "content is required")
+});
