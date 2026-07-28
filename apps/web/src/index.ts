9dd93c2f5908af6bbe9de328da3213cb4f363618
shubhook <khakhashubham@gmail.com>
2026-07-28 18:20:39 +0530

    fix: port change from '3000' to '3008'
    Prev: Both web and server were running at port:3000 now web si changed to port:3008

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -2,6 +2,7 @@ import { serve } from "bun";
 import index from "./index.html";
 
 const server = serve({
+  port: 3008,
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
